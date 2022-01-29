1bb0a65fbba31e1f719235287ac7f280af41e7da
NemaJaleel <98590086+NemaJaleel@users.noreply.github.com>
2022-01-29 11:49:04 +0530

    Update order_table.sql

--- order_table.sql
+++ order_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_booking.`order_table` (
   order_id int primary key not null,
-  `User_name` varchar(45) DEFAULT NULL,
+ 
   `Stock_name` varchar(45) DEFAULT NULL,
   `Order_quantity` int DEFAULT NULL,
   `Price` double DEFAULT NULL,
